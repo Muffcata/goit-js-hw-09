e9429f59d28f132b3839a84597155d520165eeb8
Muffcata <marta.oched@gmail.com>
2023-01-11 13:34:27 +0100

    button disabled

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -6,16 +6,22 @@ let timerId = null;
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
 const getColorBg = () => {
   timerId = setInterval(() => {
     let randomColor = getRandomHexColor();
     bgColor.style.background = randomColor;
   }, 1000);
+  buttonStart.disabled = true;
+  buttonStop.disabled;
 };
+
 buttonStart.addEventListener('click', getColorBg);
 
 const stopColorBg = () => {
   clearInterval(timerId);
+  buttonStart.disabled;
+  buttonStop.disabled = true;
 };
 
 buttonStop.addEventListener('click', stopColorBg);
