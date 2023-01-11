5065511d7f0d479f1d1a6230899b9052442c0158
Muffcata <marta.oched@gmail.com>
2023-01-11 13:05:48 +0100

    update task 1
    set random color background on click button start
    *button stop - clearInterval

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const buttonStart = document.querySelector('button[data-start="start"]');
+const buttonStop = document.querySelector('button[data-stop="stop"]');
+const bgColor = document.querySelector('body');
 
+let timerId = null;
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const getColorBg = () => {
+  timerId = setInterval(() => {
+    let randomColor = getRandomHexColor();
+    bgColor.style.background = randomColor;
+  }, 1000);
+};
+buttonStart.addEventListener('click', getColorBg);
+
+const stopColorBg = () => {
+  clearInterval(timerId);
+};
+
+buttonStop.addEventListener('click', stopColorBg);
