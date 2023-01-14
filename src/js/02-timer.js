fd07341bdb87bab67cd47fd14d51002e780eb6a6
Muffcata <marta.oched@gmail.com>
2023-01-14 21:55:13 +0100

    flatpickr - timer

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -5,7 +5,14 @@ const flatpickr = require('flatpickr');
 const fp = document.querySelector('#datetime-picker')._flatpickr;
 const currentDate = new Date().getTime();
 const buttonStart = document.querySelector('button[data-start]');
+buttonStart.disabled = true;
 
+const seconds = document.querySelector('span[data-seconds]');
+const minutes = document.querySelector('span[data-minutes]');
+const hours = document.querySelector('span[data-hours]');
+const days = document.querySelector('span[data-days]');
+
+// function to converting time
 function convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
@@ -24,6 +31,26 @@ function convertMs(ms) {
 
   return { days, hours, minutes, seconds };
 }
+const addLeadingZero = value => {
+  return (currentValue = value.toString().padStart(2, 0));
+};
+
+const countdown = () => {
+  let time = choosenTime - currentDate;
+
+  remainingTime = setInterval(() => {
+    if (time > choosenTime) {
+      clearInterval(remainingTime);
+    } else {
+      let remaining = convertMs(time);
+      days.innerHTML = addLeadingZero(remaining.days);
+      hours.innerHTML = addLeadingZero(remaining.hours);
+      minutes.innerHTML = addLeadingZero(remaining.minutes);
+      seconds.innerHTML = addLeadingZero(remaining.seconds);
+      buttonStart.disabled = true;
+    }
+  }, 1000);
+};
 
 const options = {
   enableTime: true,
@@ -31,15 +58,17 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    if (selectedDates[0] <= currentDate) {
+    if (selectedDates[0].getTime() <= currentDate) {
       buttonStart.disabled = true;
       window.alert('Please choose a date in the future');
     } else {
+      choosenTime = selectedDates[0].getTime();
+
       buttonStart.disabled = false;
     }
   },
 };
-console.log(selectedDates);
+
 flatpickr('#datetime-picker', options);
 
-buttonStart.addEventListener('click', convertMs);
+buttonStart.addEventListener('click', countdown);
