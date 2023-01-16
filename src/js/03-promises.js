a5548dc05b98fdc79f98b78018ba2a05bb202cd2
Muffcata <marta.oched@gmail.com>
2023-01-16 12:01:48 +0100

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -5,8 +5,8 @@ const firstDelay = document.querySelector('input[name="delay"]');
 const delayStep = document.querySelector('input[name="step"]');
 const amount = document.querySelector('input[name="amount"]');
 
-let delayValue = firstDelay.value;
-let stepValue = delayStep.value;
+let delayValue = firstDelay.valueAsNumber;
+let stepValue = delayStep.valueAsNumber;
 let position = 0;
 
 const fetchUserFromServer = event => {
@@ -36,19 +36,20 @@ const fetchUserFromServer = event => {
   for (let i = 1; i <= amount.value; i++) {
     position = i;
     createPromise(position, delayValue)
-      .then(value => ({ position, delayValue }) => {
+      .then(value => {
         Notiflix.Notify.success(
           `✅ Fulfilled promise ${position - 1} in ${delayValue}ms`
         );
       })
-      .catch(error => ({ position, delayValue }) => {
+      .catch(error => {
         Notiflix.Notify.failure(
           `❌ Rejected promise ${position - 1} in ${delayValue}ms`
         );
       });
+
+    delayValue = delayValue + stepValue;
+    position++;
   }
-  delayValue = +stepValue;
-  position++;
 };
 
 feedback.addEventListener('submit', fetchUserFromServer);
