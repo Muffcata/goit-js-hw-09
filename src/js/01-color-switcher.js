58e47fd92fb0d56c76089b6411578912dab9a4ad
Muffcata <marta.oched@gmail.com>
2023-01-16 10:25:38 +0100

    buttons in color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,8 +1,11 @@
 const buttonStart = document.querySelector('button[data-start]');
 const buttonStop = document.querySelector('button[data-stop]');
 const bgColor = document.querySelector('body');
+buttonStart.disabled = false;
+buttonStop.disabled = true;
 
 let timerId = null;
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
@@ -13,14 +16,14 @@ const getColorBg = () => {
     bgColor.style.background = randomColor;
   }, 1000);
   buttonStart.disabled = true;
-  buttonStop.disabled;
+  buttonStop.disabled = false;
 };
 
 buttonStart.addEventListener('click', getColorBg);
 
 const stopColorBg = () => {
   clearInterval(timerId);
-  buttonStart.disabled;
+  buttonStart.disabled = false;
   buttonStop.disabled = true;
 };
 
